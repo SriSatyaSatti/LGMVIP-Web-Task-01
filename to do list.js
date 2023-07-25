2b783b5700f392a1416d372d95fc814a331ac53e
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2023-07-25 20:13:36 +0530

    Add files via upload

---
+++ to do list.js
@@ -0,0 +1,30 @@
+var input=document.getElementById("itemadd");
+var list=document.getElementById("disp");
+function addItems(){
+    if(input.value==""){
+        alert("Type something!!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=input.value;
+        list.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        list.appendChild(span);
+        let br=document.createElement("br");
+        br.innerHTML="\n";
+        list.appendChild(br);
+        let br1=document.createElement("br");
+        br1.innerHTML="\t";
+        list.appendChild(br1);
+        let br2=document.createElement("br");
+        br2.innerHTML="\t";
+        list.appendChild(br2);
+    }
+    input.value="";
+}
+list.addEventListener("click",function(e){
+    if(e.target.tagName=="SPAN"){
+        e.target.parentElement.style.display="none";
+    }
+},false);
